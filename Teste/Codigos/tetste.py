8cde5e32fef55dd064a1170a9d94843c2c6683ad
unknown <rochacastrovictor05@gmail.com>
2026-01-12 23:30:27 -0300

    Primeiro commit: Estrutura inicial e Pandas

---
+++ Teste/Codigos/tetste.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import time
+
+dados = pd.read_csv ("https://raw.githubusercontent.com/alura-cursos/data-science-analise-exploratoria/main/Aula_0/ml-latest-small/ratings.csv")
+
+print(dados.head(50))
